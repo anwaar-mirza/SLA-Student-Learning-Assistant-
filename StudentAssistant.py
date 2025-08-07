567054e0cb69fcbdc3e47d09fd59804b27a9fbc4
anwaar-mirza <anwaar@metaviz.pro>
2025-08-07 15:49:17 +0500

    g

--- StudentAssistant.py
+++ StudentAssistant.py
@@ -17,8 +17,8 @@ import random
 import string
 import os
 load_dotenv()
-os.environ['HF_TOKEN'] = os.getenv('HF_TOKEN')
-os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['HF_TOKEN'] = st.secrets['HF_TOKEN']
+os.environ['GROQ_API_KEY'] = st.secrets['GROQ_API_KEY']
 
 
 
