87f24f1c1bc04194d4f64fa196d96ac278ec96e9
anwaar-mirza <anwaar@metaviz.pro>
2025-08-08 08:55:44 +0500

    fggg

--- StudentAssistant.py
+++ StudentAssistant.py
@@ -44,7 +44,7 @@ class StudentAssistant:
         return HuggingFaceEmbeddings(model="BAAI/bge-small-en-v1.5")
     
     def return_llm(self):
-        return ChatGroq(model="meta-llama/llama-guard-4-12b")
+        return ChatGroq(model="llama-3.3-70b-versatile", temperature=0.1, max_tokens=512)
     
     def return_vector_store(self):
         vsdb = FAISS.from_documents(self.docs, self.embeddings)
